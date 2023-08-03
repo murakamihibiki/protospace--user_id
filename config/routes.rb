948da82d68dea74de857e357dc3af40e7f94d975
murakamihibiki <rstoyo.hibiki.murakami@gmail.com>
2023-08-03 14:51:57 +0900

    変更点

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ↑↑上の記述は削除しましょう↑↑
+  root to: "view#index"
+end
